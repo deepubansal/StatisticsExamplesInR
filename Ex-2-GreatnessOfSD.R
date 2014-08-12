593d94058d13c49b21ca9f4dd13b38bed6674d5e
deepubansal <deepu.bansal@gmail.com>
2014-08-12 20:38:11 +0530

    Correct Exercise 2

--- Ex-2-GreatnessOfSD.R
+++ Ex-2-GreatnessOfSD.R
@@ -1,9 +1,12 @@
 ########################Standard Deviation Greatness#################
+library(ggplot2)
+m3 <- rnorm(n=2000, mean = 6, sd = 3)
+head(m3)
 sd <- sd(m3)
 mean <- mean(m3)
 oneSdPositive <- mean +sd
 oneSdNegative <- mean -sd
-
+qplot(m3)
 length(m3[m3 < oneSdPositive & m3 > oneSdNegative])/length(m3)
 
 twoSdPositive <- mean + 2 * sd
